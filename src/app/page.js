e068da36ad9edc44e0a29d18056c135c0d1996eb
loje0003 <loje0003@stud.kea.dk>
2026-04-16 19:18:39 +0200

    serch

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import BreedList from "../components/BreedList";
 import Search from "../components/Search";
 import { Suspense } from "react";
 
-export default function Page() {
+export default function Page({ searchParams }) {
   return (
     <div>
       <Search />
 
       <Suspense fallback={<div>Loading...</div>}>
-        <BreedList />
+        <BreedList searchParams={searchParams} />
       </Suspense>
     </div>
   );
--- src/components/BreedList.jsx
+++ src/components/BreedList.jsx
@@ -1,22 +1,25 @@
 import Card from "./Card";
 
-const BreedList = () => {
+const BreedList = ({ searchParams }) => {
   return (
     <div className="grid grid-cols-2">
-      <FetchBreed />
+      <FetchBreed searchParams={searchParams} />
     </div>
   );
 };
 
-const FetchBreed = async () => {
+const FetchBreed = async ({ searchParams }) => {
   "use server";
+  const { query } = await searchParams;
+  const url = query ? `https://api.thedogapi.com/v1/breeds/search?q=${query}` : "https://api.thedogapi.com/v1/breeds";
   try {
-    const response = await fetch("https://api.thedogapi.com/v1/breeds", {
+    const response = await fetch(url, {
       headers: {
         "x-api-key": process.env.API_KEY,
       },
     });
     const breeds = await response.json();
+
     return breeds.map((breed) => {
       return (
         <div key={breed.id}>
